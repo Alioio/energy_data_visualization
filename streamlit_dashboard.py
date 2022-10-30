70e187790b05a40499af9588af1bea18e58e6df7
Alioio <38049009+Alioio@users.noreply.github.com>
2022-10-31 00:03:50 +0100

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -555,7 +555,7 @@ date_interval = [tree_months_ago, today]
 #time_selection_column.write('**Zeitraum**')
 
 
-time_selection = st.selectbox(
+time_selection = time_selection_column.selectbox(
     label='Zeitraum: ',
     options=('1 Monat', '3 Monat', '1 Jahr', 'Eigener Zeitraum'))
 
