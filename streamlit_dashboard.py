20a490e97b6d4ea3a03870deb575acb4681b804c
Alioio <38049009+Alioio@users.noreply.github.com>
2022-11-02 13:28:19 +0100

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -725,10 +725,12 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content
 st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
 #st.radio("",("Durchschnitt","Median"))
 
+st.write(('#### Preisentwicklung - {selected_variable} und {seperation_var} der Strom - und Gastarife.').format(selected_variable=selected_variable, seperation_var=seperation_var))
+st.write('Die oberen zwei Grafiken zeigen die Entwicklung der Tarife bezüglich {selected_variable} und {seperation_var}. Im dritten Grafik ist die Anzahl der Suchanfragenergebnisse visualisiert.')
 
 if(len(date_interval) == 2):
     with electricity_chart_column:
-        chart_header = "**Preisentwicklung - {energy_selection}verträge ({selected_variable})**".format(selected_variable=selected_variable, energy_selection='Strom')
+        chart_header = "**{energy_selection}verträge ({selected_variable})**".format(selected_variable=selected_variable, energy_selection='Strom')
         summary_3000 = summarize(electricity_results_3000, seperation_var, int(selection_slider),'3000',selected_variable)
         summary_1300 = summarize(electricity_results_1300, seperation_var, int(selection_slider),'1300', selected_variable)
         summary = pd.concat([summary_3000, summary_1300])
@@ -741,7 +743,7 @@ if(len(date_interval) == 2):
         with tariff_list_expander:
             st.info('Hier ist gedacht die Tarife aufzulisten die oben im Barchart ausgewählt sind')
     with gas_chart_column:
-        chart_header = "**Preisentwicklung - {energy_selection}verträge ({selected_variable})**".format(selected_variable=selected_variable, energy_selection='Gas')
+        chart_header = "**{energy_selection}verträge ({selected_variable})**".format(selected_variable=selected_variable, energy_selection='Gas')
         summary_9000 = summarize(gas_results_9000, seperation_var,int(selection_slider),'9000',selected_variable)
         summary_15000 = summarize(gas_results_15000, seperation_var,int(selection_slider),'15000',selected_variable)
         summary = pd.concat([summary_9000, summary_15000])
